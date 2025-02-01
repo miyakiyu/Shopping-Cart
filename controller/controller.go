b3de7437992b9f114d9274e4e39c33a259ab1efb
miyakiyu <sherry910509@gmail.com>
2025-02-01 14:25:13 +0800

    Fixing bug

--- controller/controller.go
+++ controller/controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	products "shop/model/product"
 	"shop/model/user"
 
 	"github.com/gin-gonic/gin"
@@ -50,10 +51,60 @@ func UserLogin(c *gin.Context, db *gorm.DB) {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
+	//Setting cookie
+	c.SetCookie("user", user.Username, 3600, "/", "localhost", false, true)
+	c.SetCookie("role", user.Role, 3600, "/", "localhost", false, true)
+
 	c.JSON(http.StatusOK, gin.H{"message": "Login success!"})
 }
 
-// Show all products
-func GetAllProducts(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "pong"})
+// Add new product
+func AddProduct(c *gin.Context, db *gorm.DB) {
+	var input struct {
+		Name  string `json:"name" binding:"required"`
+		Price int    `json:"price" binding:"required"`
+		Stock int    `json:"stock" binding:"required"`
+	}
+	// Check input
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "不是json"})
+		return
+	}
+	// Check role
+	role, exist := c.Get("role")
+	if !exist || role != "seller" {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Permission denied"})
+		return
+	}
+	// Product to database
+	product := products.Product{
+		Name:  input.Name,
+		Price: input.Price,
+		Stock: input.Stock,
+	}
+	if err := db.Create(&product).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "database錯誤"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Product added!"})
+
+}
+
+// Check user role
+func UserInfo(c *gin.Context) {
+	user, exists := c.Get("user")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"logged_in": true,
+		"username":  user.(string),
+		"role":      c.GetString("role"),
+	})
+}
+
+// Show all products from database
+func GetAllProducts(c *gin.Context, db *gorm.DB) {
+
 }
